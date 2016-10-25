44a0164547cfb1d80c8fea540ae10b018a1a541d
Christopher Augustus <repos@c4augustus.com>
2016-10-25 12:26:26 +0100

    added plugin struct and impls

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,27 @@
 //  License, v. 2.0. If a copy of the MPL was not distributed with this
 //  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+extern crate rustvr_core;
 #[macro_use]
 extern crate gfx; // !!! must be declared here due to #[macro_use]
 
 pub mod terrain;
+
+pub struct PluginGlGfx {
+}
+
+impl rustvr_core::Plugin for PluginGlGfx {
+}
+
+impl rustvr_core::PluginGl for PluginGlGfx {
+    fn run_terrain(&self) {
+        ::terrain::main()
+    }
+}
+
+pub fn make_plugin() -> PluginGlGfx {
+    PluginGlGfx {}
+}
 
 #[cfg(test)]
 mod tests {
