90ebc375a92507a8446eb100f1da1449b38fa11d
Christopher Augustus <repos@c4augustus.com>
2016-10-25 12:25:13 +0100

    copied gfx example terrain into a module

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,21 @@
+//  Copyright 2016 RustVR
+//
+//  This Source Code Form is subject to the terms of the Mozilla Public
+//  License, v. 2.0. If a copy of the MPL was not distributed with this
+//  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#[macro_use]
+extern crate gfx; // !!! must be declared here due to #[macro_use]
+
+pub mod terrain;
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
+    }
+    #[test]
+    fn terrain_main() {
+        super::terrain::main();
     }
 }
